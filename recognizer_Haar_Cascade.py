5af33ccc68473f2c0e6a45b5e764d2118079d355
marouane05 <marouan.mdr@gmail.com>
2023-01-21 21:11:09 +0100

    second commit

--- recognizer_Haar_Cascade.py
+++ recognizer_Haar_Cascade.py
@@ -15,8 +15,11 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 # iniciate id counter
 id = 0
 # names related to ids: example ==> Marcelo: id=1,  etc
-#names = ['none','Marouane', 'Obama','ismail ','karima']
-new_df = pd.read_csv('./data/persons.csv')
+names = ['none','Marouane', 'Obama']
+
+#new_df = pd.read_csv('./data/persons.csv')
+
+
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
 cam.set(3, 640)  # set video widht
@@ -27,61 +30,44 @@ minH = 0.1 * cam.get(4)
 while True:
     ret, img = cam.read()
     #img = cv2.flip(img, -1)  # Flip vertically
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale( 
         gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(int(minW), int(minH)),
-    )
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        timing=time.time()
-        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-        print("done in % 0.3fs" % (time.time()-timing))
-        subid=''
-        # If confidence is less them 100 ==> "0" : perfect match
-        if (confidence < 70):
-
-            df_res = new_df.loc[new_df['id'] == int(id)];
-            print(df_res.to_numpy()[0])
-            arr = df_res.to_numpy()[0]
-            id = arr[1]
-            subid=arr[2]
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        
+        # If confidence is less them 100 ==> "0" : perfect match 
+        if (confidence < 100):
+            id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
-            id = "inconnu"
+            id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
-
+        
         cv2.putText(
-            img,
-            str(id),
-            (x + 3, y - 5),
-            font,
-            0.7,
-            (255, 255, 255),
-            2
-        )
+                    img, 
+                    str(id), 
+                    (x+5,y-5), 
+                    font, 
+                    1, 
+                    (255,255,255), 
+                    2
+                   )
         cv2.putText(
-            img,
-            str(subid),
-            (x + 5, y + h+20),
-            font,
-            0.5,
-            (255, 255, 255),
-            2
-        )
-
-        cv2.putText(
-            img,
-            str(confidence),
-            (x + 5, y + h - 5),
-            font,
-            1,
-            (255, 255, 0),
-            1
-        )
+                    img, 
+                    str(confidence), 
+                    (x+5,y+h-5), 
+                    font, 
+                    1, 
+                    (255,255,0), 
+                    1
+                   )  
 
     cv2.imshow('camera', img)
     k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
